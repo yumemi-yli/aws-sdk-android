7da41a7c5bb7998dab672a1aa184ca20605d5e68
Min Bi <min.bi.888@gmail.com>
2019-02-06 11:32:33 -0800

    Add sign out options

--- aws-android-sdk-mobile-client/src/main/java/com/amazonaws/mobile/client/AWSMobileClient.java
+++ aws-android-sdk-mobile-client/src/main/java/com/amazonaws/mobile/client/AWSMobileClient.java
@@ -814,6 +814,9 @@ public final class AWSMobileClient implements AWSCredentialsProvider {
         };
     }
 
+    /**
+     * Clears local tokens so that the client is in a signed-out state.
+     */
     public void signOut() {
         mCognitoUserSession = null;
         if (userpool != null) {
@@ -828,6 +831,12 @@ public final class AWSMobileClient implements AWSCredentialsProvider {
         mStore.clear();
         setUserState(getUserStateDetails(false));
         releaseSignInWait();
+    }
+
+    public void signOut(final SignOutOptions signOutOptions) {
+        if (signOutOptions.isSignOutGlobally()) {
+
+        }
     }
 
     /**
