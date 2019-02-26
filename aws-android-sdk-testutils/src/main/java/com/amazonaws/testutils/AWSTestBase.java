a9de5f65222a344230046a23dbd948b837ac2e19
Roshan Kumar <roskumr@amazon.com>
2019-02-26 06:46:31 -0800

    Update AWSTestBase.java

--- aws-android-sdk-testutils/src/main/java/com/amazonaws/testutils/AWSTestBase.java
+++ aws-android-sdk-testutils/src/main/java/com/amazonaws/testutils/AWSTestBase.java
@@ -48,6 +48,8 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
+import org.junit.runner.RunWith;
+import android.support.test.runner.AndroidJUnit4;
 
 @RunWith(AndroidJUnit4.class)
 public abstract class AWSTestBase {
